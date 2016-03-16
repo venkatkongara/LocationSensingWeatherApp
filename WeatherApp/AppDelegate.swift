08e2e114c9a3a829ed19d64036a346c26a721cc1
Venkat Kongara <venkat.kongara@mch.com>
2016-03-15 21:50:36 -0400

    fixes

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        let settings = UIUserNotificationSettings(forTypes: [.Sound,.Alert], categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
         return true
     }
 
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -3,8 +3,10 @@
 //  WeatherApp
 //
 //  Created by siva kongara on 1/7/16.
-//  Copyright © 2016 siva kongara. All rights reserved.
+//  Copyright © 2016 Venkat kongara. All rights reserved.
 //
+
+// Note some Cities might not be supported by the Web API. SO it might show some city in the near longititude
 
 import UIKit
 import CoreLocation
@@ -22,6 +24,9 @@ class ViewController: UIViewController {
     let locationManger = CLLocationManager()
     var forecastData = [String]()
     var city = ""
+    let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .Gray)
+    var lastLocationLattitude: Double?
+    var lastLocationLongitude: Double?
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -34,10 +39,22 @@ class ViewController: UIViewController {
         locationManger.requestAlwaysAuthorization()
         locationManger.startUpdatingLocation()
         tableView.alpha = 0
+        activityIndicator.startAnimating()
+        activityIndicator.frame = view.bounds
+        view.addSubview(activityIndicator)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func fireLocalNotificationNow() {
+        let notification = UILocalNotification()
+        notification.alertTitle = "Location chaged"
+        notification.hasAction = true
+        notification.alertBody = "Updated the weekly weather forecast"
+        notification.fireDate = NSDate()
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
     }
 
 }
@@ -45,13 +62,28 @@ class ViewController: UIViewController {
 extension ViewController: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
+        let location = locations.first
         lattitude = "\(location!.coordinate.latitude)"
         longitude = "\(location!.coordinate.longitude)"
-        forecastData = WeatherFromAPI().getWeekForecast(lattitude, longitude: longitude).0
-        city = WeatherFromAPI().getWeekForecast(lattitude, longitude: longitude).1
-        tableView.reloadData()
-        tableView.alpha = 1
+        
+        if (lastLocationLattitude != location?.coordinate.latitude) && (lastLocationLongitude != location?.coordinate.longitude) {
+            lastLocationLattitude = location?.coordinate.latitude
+            lastLocationLongitude = location?.coordinate.longitude
+            fireLocalNotificationNow()
+        }
+        
+        // Does the background fetching and works even when app is in background
+        dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)) {[weak self] () -> Void in
+            self?.forecastData = WeatherFromAPI().getWeekForecast((self?.lattitude)!, longitude: (self?.longitude)!).0
+            self?.city = WeatherFromAPI().getWeekForecast((self?.lattitude)!, longitude: (self?.longitude)!).1
+            
+            // updates UI on the main queue when back ground task is completed
+            dispatch_async(dispatch_get_main_queue(), {[weak self] () -> Void in
+                self?.tableView.reloadData()
+                self?.tableView.alpha = 1
+                self?.activityIndicator.stopAnimating()
+            })
+        }
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
@@ -93,8 +125,8 @@ extension ViewController: UITableViewDataSource {
             let nextDate = theCalendar.dateByAddingComponents(dayComponent, toDate: NSDate(), options: .MatchStrictly)
             let dateString = "\(nextDate!)"
             let range = dateString.startIndex..<dateString.startIndex.advancedBy(11)
-            tableViewCell?.city.text = city + " " + dateString[range]
-            tableViewCell?.temperatureHigh.text = forecastData[indexPath.row]
+            tableViewCell?.city.text = city + "    " + dateString[range]
+            tableViewCell?.temperatureHigh.text = "\(round(Float(forecastData[indexPath.row])!))"
         }
         return tableViewCell!
     }
